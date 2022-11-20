d070591c92160125a8dcb8caca8f623daea6eb7a
AAGaming <aa@mail.catvibers.me>
2022-11-19 22:25:32 -0500

    init

--- backend/loader.py
+++ backend/loader.py
@@ -82,6 +82,7 @@ class Loader:
             self.loop.create_task(self.enable_reload_wait())
 
         server_instance.add_routes([
+            web.get("/socket", self.handle_ws),
             web.get("/frontend/{path:.*}", self.handle_frontend_assets),
             web.get("/plugins", self.get_plugins),
             web.get("/plugins/{plugin_name}/frontend_bundle", self.handle_frontend_bundle),
@@ -93,6 +94,24 @@ class Loader:
             web.get("/plugins/plugin_resource/{name}/{path:.+}", self.handle_sub_route),
             web.get("/steam_resource/{path:.+}", self.get_steam_resource)
         ])
+
+    async def handle_ws(self, request):
+        self.logger.debug('Websocket connection starting')
+        ws = aiohttp.web.WebSocketResponse()
+        await ws.prepare(request)
+        self.logger.debug('Websocket connection ready')
+
+        async for msg in ws:
+            self.logger.debug(msg)
+            if msg.type == aiohttp.WSMsgType.TEXT:
+                self.logger.debug(msg.data)
+                if msg.data == 'close':
+                    await ws.close()
+                else:
+                    await ws.send_str(msg.data + '/answer')
+
+        self.logger.debug('Websocket connection closed')
+        return ws
 
     async def enable_reload_wait(self):
         if self.live_reload:
